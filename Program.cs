cfca3f66105bfbe2c27df307d0c568b4ef879149
lukewangshape <luke.wang@shape.co.nz>
2024-04-22 17:05:43 +1200

    Progress on AddSlaveToNetworkWithParseMap

--- Program.cs
+++ Program.cs
@@ -4,6 +4,15 @@ using System.Drawing.Printing;
 using Newtonsoft.Json;
 using System.ComponentModel;
 using ModbusDecoderTemp;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Diagnostics.Metrics;
+using System.Drawing;
+using System.Net;
+using System.Numerics;
+using System.Security.Cryptography;
+using System.Text;
+using System.Xml.Linq;
+using System;
 
 
 
@@ -165,7 +174,7 @@ static Dictionary<string, dynamic> ParseBasicADURequestHexString(string aduHexSt
     return modbusRequest;
 }
 
-static void AddSlaveToNetworkWithParseMap(dynamic networkMap, string bus, string address, string name, string description, string registerMap, string parseMap)
+static Dictionary<string, dynamic> AddSlaveToNetworkWithParseMap(dynamic networkMap, string bus, string address, string name, string description, dynamic registerMap, dynamic parseMap)
 {
     Dictionary<string, dynamic> slave = new Dictionary<string, dynamic>();
     var networkMapTable = networkMap;
@@ -176,8 +185,10 @@ static void AddSlaveToNetworkWithParseMap(dynamic networkMap, string bus, string
     slave["ParseMap"] = parseMap;
     if (networkMapTable["Bus"] == null)
     {
-        //c
+        networkMapTable["Bus"] = new Dictionary<string, dynamic>();
     }
+    networkMapTable["Bus"]["Address"] = slave;
+    return networkMapTable;
 }
 
 
@@ -189,8 +200,21 @@ static void AddSlaveToNetworkWithParseMap(dynamic networkMap, string bus, string
 //    Console.WriteLine(b.ToString());
 //}
 
-
 //var parsedRTUResponse = ParseBasicADUResponseHexString("01030A2FF80000006A48B800013864", "RTU");
 var parsedRTUResponse = ParseBasicADURequestHexString("004000000006010300000023", "TCP");
-Console.WriteLine(JsonConvert.SerializeObject(parsedRTUResponse, Formatting.Indented));
+//Console.WriteLine(JsonConvert.SerializeObject(parsedRTUResponse, Formatting.Indented));
 
+Dictionary<string, dynamic> ModbusMap = new Dictionary<string, dynamic> {
+    //{ "MOD_MAP_CONXIONS_WATER_METER_V1", "{\"MODBUS_MAP\":{\"2\":{\"HOLDING_REG\":{\"Units\":\"M3/H\", \"Writeable\":false, \"Description\":\"Instantaneous Flow rate\", \"Name\":\"Flow Rate\", \"Size\":32, \"Readable\":true, \"Encoding\":\"UINT32\",\"_Current\":true,\"_StartTime\":1593732266,\"_EndTime\":0,\"_Id\":1593732266}}, \"4\":{\"HOLDING_REG\":{\"Units\":\"C\", \"Writeable\":false, \"Description\":\"Instantaneous Water Temperature\", \"Name\":\"Water Temperature\", \"Size\":16, \"Readable\":true, \"Encoding\":\"UINT16\",\"_Current\":true,\"_StartTime\":1593732267,\"_EndTime\":0,\"_Id\":1593732267}}, \"5\":{\"HOLDING_REG\":{\"Units\":\"ss\", \"Writeable\":false, \"Description\":\"Time Second\", \"Name\":\"Second\", \"Size\":16, \"Readable\":true, \"Encoding\":\"UINT16\",\"_Current\":true,\"_StartTime\":1593732268,\"_EndTime\":0,\"_Id\":1593732268}}, \"6\":{\"HOLDING_REG\":{\"Units\":\"mm\", \"Writeable\":false, \"Description\":\"Time Minute\", \"Name\":\"Minute\", \"Size\":16, \"Readable\":true, \"Encoding\":\"UINT16\",\"_Current\":true,\"_StartTime\":1593732269,\"_EndTime\":0,\"_Id\":1593732269}}, \"7\":{\"HOLDING_REG\":{\"Units\":\"hh\", \"Writeable\":false, \"Description\":\"Time Hour\", \"Name\":\"Hour\", \"Size\":16, \"Readable\":true, \"Encoding\":\"UINT16\",\"_Current\":true,\"_StartTime\":1593732270,\"_EndTime\":0,\"_Id\":1593732270}}, \"8\":{\"HOLDING_REG\":{\"Units\":\"DD\", \"Writeable\":false, \"Description\":\"Time Day\", \"Name\":\"Day\", \"Size\":16, \"Readable\":true, \"Encoding\":\"UINT16\",\"_Current\":true,\"_StartTime\":1593732271,\"_EndTime\":0,\"_Id\":1593732271}}, \"9\":{\"HOLDING_REG\":{\"Units\":\"MM\", \"Writeable\":false, \"Description\":\"Time Month\", \"Name\":\"Month\", \"Size\":16, \"Readable\":true, \"Encoding\":\"UINT16\",\"_Current\":true,\"_StartTime\":1593732272,\"_EndTime\":0,\"_Id\":1593732272}}, \"10\":{\"HOLDING_REG\":{\"Units\":\"YYYY\", \"Writeable\":false, \"Description\":\"Time Year\", \"Name\":\"Year\", \"Size\":16, \"Readable\":true, \"Encoding\":\"UINT16\",\"_Current\":true,\"_StartTime\":1593732273,\"_EndTime\":0,\"_Id\":1593732273}}, \"11\":{\"HOLDING_REG\":{\"Units\":\"b\", \"Writeable\":false, \"Description\":\"Status\", \"Name\":\"Status\", \"Size\":16, \"Readable\":true, \"Encoding\":\"UINT16\",\"_Current\":true,\"_StartTime\":1593732274,\"_EndTime\":0,\"_Id\":1593732274}}, \"16\":{\"HOLDING_REG\":{\"Units\":\"a\", \"Writeable\":true, \"Description\":\"Slave Address\", \"Name\":\"Slave Address\", \"Size\":16, \"Readable\":true, \"Encoding\":\"UINT16\",\"_Current\":true,\"_StartTime\":1593732275,\"_EndTime\":0,\"_Id\":1593732275}}, \"0\":{\"HOLDING_REG\":{\"Units\":\"M3\", \"Writeable\":false, \"Description\":\"Meter Reading\", \"Name\":\"Total Consumption\", \"Size\":32, \"Readable\":true, \"Encoding\":\"UINT32\",\"_Current\":true,\"_StartTime\":1593732276,\"_EndTime\":0,\"_Id\":1593732276}}}}"}
+    { "MOD_MAP_CONXIONS_WATER_METER_V1", """{"MODBUS_MAP":{"2":{"HOLDING_REG":{"Units":"M3/H", "Writeable":false, "Description":"Instantaneous Flow rate", "Name":"Flow Rate", "Size":32, "Readable":true, "Encoding":"UINT32","_Current":true,"_StartTime":1593732266,"_EndTime":0,"_Id":1593732266}}, "4":{"HOLDING_REG":{"Units":"C", "Writeable":false, "Description":"Instantaneous Water Temperature", "Name":"Water Temperature", "Size":16, "Readable":true, "Encoding":"UINT16","_Current":true,"_StartTime":1593732267,"_EndTime":0,"_Id":1593732267}}, "5":{"HOLDING_REG":{"Units":"ss", "Writeable":false, "Description":"Time Second", "Name":"Second", "Size":16, "Readable":true, "Encoding":"UINT16","_Current":true,"_StartTime":1593732268,"_EndTime":0,"_Id":1593732268}}, "6":{"HOLDING_REG":{"Units":"mm", "Writeable":false, "Description":"Time Minute", "Name":"Minute", "Size":16, "Readable":true, "Encoding":"UINT16","_Current":true,"_StartTime":1593732269,"_EndTime":0,"_Id":1593732269}}, "7":{"HOLDING_REG":{"Units":"hh", "Writeable":false, "Description":"Time Hour", "Name":"Hour", "Size":16, "Readable":true, "Encoding":"UINT16","_Current":true,"_StartTime":1593732270,"_EndTime":0,"_Id":1593732270}}, "8":{"HOLDING_REG":{"Units":"DD", "Writeable":false, "Description":"Time Day", "Name":"Day", "Size":16, "Readable":true, "Encoding":"UINT16","_Current":true,"_StartTime":1593732271,"_EndTime":0,"_Id":1593732271}}, "9":{"HOLDING_REG":{"Units":"MM", "Writeable":false, "Description":"Time Month", "Name":"Month", "Size":16, "Readable":true, "Encoding":"UINT16","_Current":true,"_StartTime":1593732272,"_EndTime":0,"_Id":1593732272}}, "10":{"HOLDING_REG":{"Units":"YYYY", "Writeable":false, "Description":"Time Year", "Name":"Year", "Size":16, "Readable":true, "Encoding":"UINT16","_Current":true,"_StartTime":1593732273,"_EndTime":0,"_Id":1593732273}}, "11":{"HOLDING_REG":{"Units":"b", "Writeable":false, "Description":"Status", "Name":"Status", "Size":16, "Readable":true, "Encoding":"UINT16","_Current":true,"_StartTime":1593732274,"_EndTime":0,"_Id":1593732274}}, "16":{"HOLDING_REG":{"Units":"a", "Writeable":true, "Description":"Slave Address", "Name":"Slave Address", "Size":16, "Readable":true, "Encoding":"UINT16","_Current":true,"_StartTime":1593732275,"_EndTime":0,"_Id":1593732275}}, "0":{"HOLDING_REG":{"Units":"M3", "Writeable":false, "Description":"Meter Reading", "Name":"Total Consumption", "Size":32, "Readable":true, "Encoding":"UINT32","_Current":true,"_StartTime":1593732276,"_EndTime":0,"_Id":1593732276}}}}"""}
+};
+
+var networkConfig = new Dictionary<string, dynamic>();
+//var waterMeterMap = ModbusMap["MOD_MAP_CONXIONS_WATER_METER_V1"];
+Dictionary<string, dynamic> waterMeterMap = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(ModbusMap["MOD_MAP_CONXIONS_WATER_METER_V1"]);
+//waterMeterMap = waterMeterMap.First();
+//var modbusMap = waterMeterMap["MODBUS_MAP"];
+//var parse = waterMeterMap["Parse"];
+//networkConfig = AddSlaveToNetworkWithParseMap(networkConfig, "BUS_A", "2", "Conxions", "Test Modbus Map", waterMeterMap["MODBUS_MAP"], waterMeterMap["Parse"]);
+networkConfig = AddSlaveToNetworkWithParseMap(networkConfig, "BUS_A", "2", "Conxions", "Test Modbus Map", waterMeterMap["MODBUS_MAP"], null);
+Console.WriteLine(JsonConvert.SerializeObject(networkConfig, Formatting.Indented));
